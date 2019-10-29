4ad75eb366c67b043b5dd280e355fe22354326c0
Torment <56950420+MdZubaerHasan@users.noreply.github.com>
2019-10-29 19:50:25 +0600

    Add files via upload

---
+++ HelloWorld.py
@@ -0,0 +1,49 @@
+# print('Hello World')
+# print('hello '+'world')
+# splistring = 'this string has been\nsplit over\nseveral times'
+# print(splistring)
+# tabbedString = 't1\tt2\tt3\tt4\tt5\tt6'
+# print(tabbedString)
+# print('The pet shop owner said "No , no , \'e\'s uh,..he\'s resting"')
+# print("The pet shop owner said \" No , no, 'e's uh,... he's resting\"")
+# print('''no no
+# you are de snake
+# and we all know it ''')
+# mew = 'meow'
+# maw = 1
+# a = 12
+# b = 3
+# print(a+b)
+# print(a-b)
+# print(a*b)
+# print(a/b)
+# print(a//b)
+# print(a%b)
+# print(a**b)
+# for i in range(1 , a//b):
+#     print(i)
+# print(a+b/3-4*12)
+# cat = ' Big Phat Cat '
+# print(cat[0:6:3])
+# number = '123456789'
+# print(number[1:10:2])
+# print('hello ' * (5 + 4) )
+# today = 'Friday'
+# print('day' in today)
+# print('poo' in today)
+# age = 24
+# print("My age is " + str(age) + " years")
+# print("My age is {0} years ".format(age))
+# print("there are {0} days in  {1} , {2} ,{3} ,{4}" .format(31,"jan", "feb" , " mar", "april"))
+# print("my age is %d years" %age)
+# print("My age  is %d %s %d %s "%(21,"years", 10 , "months"))
+#  for i in range(1,12):
+#      print("No. %2d squared is %4d and cubed is %4d" %(i,i**2,i**3)) #%2d %4 d allocates space
+# print("Pi is approximately %6.50f" %(22/7))
+# for i in range(1, 12):
+#     print("No. {0:<2} squared is {1:<4} and cubed is {2:<4}" .format(i, i**2, i**3))  # %2d %4 d allocates space
+#print("my name is {0:12.50}".format(22/7))
+# m = 13
+# n = 14
+# print("m =", m, " n =", n)
+# print('Hello, your balance is {0:^20.10}'.format(450.650369646441))
